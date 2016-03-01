69810efe17b5760466ac66d379b9dc1f1580662c
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-03-01 09:56:05 +0200

    Added users service.

---
+++ app/services/users-service.js
@@ -0,0 +1,25 @@
+'use strict';
+let applicationSettings = require('application-settings');
+let constants = require('../common/constants');
+
+class UsersService {
+  saveCurrentUserInLocalStorage(user) {
+    let promise = new Promise(function (resolve, reject) {
+      applicationSettings.setString(constants.APPLICATION_SETTINGS_USER_KEY, JSON.stringify(user));
+      
+      resolve(user);
+    });
+    
+    return promise;
+  }
+  
+  getCurrentUserFromLocalStorage() {
+    let promise = new Promise(function (resolve, reject) {
+      let jsonUser = applicationSettings.getString(constants.APPLICATION_SETTINGS_USER_KEY);
+      
+      resolve(JSON.parse(jsonUser));
+    });
+    
+    return promise;
+  }
+}
