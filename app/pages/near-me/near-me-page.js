5da58f838efa2a159767237469f812f9ccba8573
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-03-05 14:03:17 +0200

    Added get tourist sites near me using nativescript-geolocation module to get the current location.

--- app/pages/near-me/near-me-page.js
+++ app/pages/near-me/near-me-page.js
@@ -2,7 +2,9 @@
 
 let nearMeViewModel = require('./near-me-view-model');
 let customAnimations = require('../../common/custom-animations');
+let loader = require('nativescript-loading-indicator');
 let frame = require('ui/frame');
+let dialogs = require('ui/dialogs');
 let viewModel;
 
 function pageLoaded(args) {
@@ -11,14 +13,36 @@ function pageLoaded(args) {
 
   page.bindingContext = viewModel;
 
+  loader.show();
+
   viewModel.findTouristSitesNearMe()
     .then(function (data) {
-      console.dir(data);
+      loader.hide();
     }, function (err) {
-      console.dir(err);
+      loader.hide();
+      dialogs.alert({
+        title: 'Error',
+        message: 'Cannot find your location or there is no connection to the server.',
+        okButtonText: 'OK'
+      });
+    });
+}
+
+function searchBtnTap(args) {
+  let button = args.object;
+  button.animateTap();
+
+  loader.show();
+
+  viewModel.findTouristSitesNearMe()
+    .then(function (data) {
+      loader.hide();
+    }, function (err) {
+      loader.hide();
     });
 }
 
 module.exports = {
-  pageLoaded
+  pageLoaded,
+  searchBtnTap
 };
--- app/pages/near-me/near-me-view-model.js
+++ app/pages/near-me/near-me-view-model.js
@@ -1,17 +1,53 @@
 'use strict';
 
 let Observable = require('data/observable').Observable;
-let users = require('../../helpers/users-helper').defaultInstance;
+let ObservableArray = require('data/observable-array').ObservableArray;
+let geolocation = require('nativescript-geolocation');
+let touristSitesService = require('../../web/services/tourist-sites-service').defaultInstance;
+let enums = require('ui/enums');
 
 class NearMeViewModel extends Observable {
   constructor() {
     super();
     this.title = 'Near me';
+    this.radius = 10;
+    this.touristSites = new ObservableArray([]);
   }
 
   findTouristSitesNearMe() {
-    let promise = new Promise(function (resolve, reject) {
+    let that = this;
+    this.touristSites.splice(0, this.touristSites.length);
 
+    let promise = new Promise(function (resolve, reject) {
+      if (!geolocation.isEnabled()) {
+        geolocation.enableLocationRequest();
+        resolve(true);
+        return;
+      }
+
+      geolocation.getCurrentLocation({
+        desiredAccuracy: enums.Accuracy.high,
+        updateDistance: 10,
+        maximumAge: 3000,
+        timeout: 20 * 1000
+      }).
+        then(function (location) {
+          console.log('-------Location');
+          return touristSitesService.getTouristSitesNearPosition(location, that.radius);
+        }, function (err) {
+          console.log('-------Error when getting the current location.');
+          console.log(err);
+          reject({
+            message: err
+          });
+        })
+        .then(function (data) {
+          data.forEach(function (item) {
+            that.touristSites.push(item);
+          });
+
+          resolve(true);
+        }, reject);
     });
 
     return promise;
