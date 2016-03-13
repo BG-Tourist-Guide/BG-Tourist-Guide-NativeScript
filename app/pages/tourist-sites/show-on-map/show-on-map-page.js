725a3a093ab57fbc8f5faef0fa638bfc934e83b6
TsvetanMilanov <tsvetan.milanov17@gmail.com>
2016-03-13 09:54:31 +0200

    Added show on map functionality which needs to be tested.

--- app/pages/tourist-sites/show-on-map/show-on-map-page.js
+++ app/pages/tourist-sites/show-on-map/show-on-map-page.js
@@ -1,18 +1,24 @@
 'use strict';
-
-let nearMeViewModel = require('./near-me-view-model');
+let showOnMapViewModel = require('./show-on-map-view-model');
 let customAnimations = require('../../../common/custom-animations');
 let loader = require('nativescript-loading-indicator');
 let frame = require('ui/frame');
 let dialogs = require('ui/dialogs');
 let isBackNavigation = false;
-let viewModel;
+let viewModel = showOnMapViewModel.defaultInstance;
 
 function pageLoaded(args, a) {
   let page = args.object;
+  let touristSite = args.context;
+  
+  viewModel = new showOnMapViewModel.ShowOnMapViewModel(touristSite);
+}
 
+function onMapReady(args) {
+  viewModel.onMapReady(args);
 }
 
 module.exports = {
-  pageLoaded
+  pageLoaded,
+  onMapReady
 };
--- app/pages/tourist-sites/show-on-map/show-on-map-view-model.js
+++ app/pages/tourist-sites/show-on-map/show-on-map-view-model.js
@@ -1,10 +1,25 @@
 'use strict';
 
 let Observable = require('data/observable').Observable;
+let mapsModule = require('nativescript-google-maps-sdk');
 
 class ShowOnMapViewModel extends Observable {
-  constructor() {
+  constructor(touristSite) {
     super();
+
+    this.touristSite = touristSite;
+  }
+
+  onMapReady(args) {
+    let mapView = args.object;
+
+    console.log("-------Setting a marker...");
+    let marker = new mapsModule.Marker();
+    marker.position = mapsModule.Position.positionFromLatLng(-33.86, 151.20);
+    marker.title = "Sydney";
+    marker.snippet = "Australia";
+    marker.userData = { index: 1 };
+    mapView.addMarker(marker);
   }
 }
 
